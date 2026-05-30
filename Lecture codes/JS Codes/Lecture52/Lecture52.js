d8dc00d5ee0a310b1e425d3e875ad9b468464d03
atharvaBOT7 <atharvachundurwar841@gmail.com>
2026-05-30 14:01:22 -0700

    Lecture 52 CSS DOM Manipulation JS HTML code and notes added

---
+++ Lecture codes/JS Codes/Lecture52/Lecture52.js
@@ -0,0 +1,23 @@
+// let newElement = document.querySelector("#spara");
+
+// newElement.style.backgroundColor = "red";
+
+// let newElement = document.querySelector("#spara");
+
+// newElement.style.cssText = "background-color: black; color: white; padding: 0.3rem";
+
+// let firstElement = document.getElementById("fdiv");
+
+// firstElement.setAttribute("class", "divClass");
+
+
+// let fpara = document.querySelector("#fpara");
+
+// fpara.classList;      // Outputs this: DOMTokenList(2) ['firstClass', 'secondClass', value: 'firstClass secondClass']
+
+// fpara.classList.add("thirdClass");     // Adds another class, appends not replaces. Therefore we will see 3 classes now
+
+// fpara.classList.remove("thirdClass");    // Removes the thirdClass
+
+// fpara.classList.toggle("firstClass");    // Toggle means if the class is present then we remove it and if the class is not present then it will add that class, just like the not operator.
+// fpara.classList.toggle("firstClass");    // This will again add the firstClass because the first toggle removed it.
