335511c19c50467c94926e9b8c5f1454cdc2f817
atharvaBOT7 <atharvachundurwar841@gmail.com>
2026-06-02 21:46:48 -0700

    Lecture 60 notes and react first project code added

---
+++ react-project-one/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p> I am Atharva </p>
+    </div>
+  )
+}
+
+export default App
